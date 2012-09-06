89fabbf71cb91cc477c612b7ab9da386194c58ae
Ilya Shpigor <petrsum@gmail.com>
2012-09-06 15:14:53 +0400

    Rename the virtual controller a20 scenario

--- bin/start.sh
+++ bin/start.sh
@@ -2,4 +2,4 @@
 
 ./clean.sh
 ./virt-controller --ipl 192.168.0.101 --ipr 192.168.0.101 --portl 4002 --portr 4001 \
-                  --rs 1 --scenario a20-kts25sm-3-4001-1.vdc > debug.log
+                  --rs 1 --scenario a20-init.vdc > debug.log
