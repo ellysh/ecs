930d111da8e898929bfd3e93609d13140325c39e
Ilya Shpigor <petrsum@gmail.com>
2012-09-06 14:34:45 +0400

    Add the VirtualController class

---
+++ source/virtual_controller.cpp
@@ -0,0 +1,8 @@
+#include "virtual_controller.h"
+
+using namespace virt_dashboard;
+
+void VirtualController::Start()
+{
+    /* FIXME: Implement this method */
+}
---
+++ source/virtual_controller.h
@@ -0,0 +1,20 @@
+#ifndef VIRTUAL_CONTROLLER_H
+#define VIRTUAL_CONTROLLER_H
+
+#include <string>
+#include <list>
+
+#include "types_vdb.h"
+
+namespace virt_dashboard
+{
+
+class VirtualController
+{
+public:
+    void Start();
+};
+
+}
+
+#endif
