f90ad496104642b6f71efdb200918ab0690bbd1f
Ilya Shpigor <petrsum@gmail.com>
2012-09-07 11:00:11 +0400

    Complete the a20 controller initialize scenario

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./clean.sh
-./virt-controller --ipl 192.168.0.101 --ipr 192.168.0.101 --portl 4002 --portr 4001 \
+./virt-controller --ipl 127.0.0.1 --ipr 127.0.0.1 --portl 4002 --portr 4001 \
                   --rs 1 --scenario a20-init.vdc > debug.log
