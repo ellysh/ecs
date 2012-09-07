71b4d97cece0a3d7f413ab1c03f9a358229bb68d
Ilya Shpigor <petrsum@gmail.com>
2012-09-07 16:28:49 +0400

    ControllerParser: Move the ParseRequest, StringToByte and StringToArray methods to base class

--- source/core/scenario_parser.h
+++ source/core/scenario_parser.h
@@ -13,10 +13,14 @@ class ScenarioParser
 public:
     virtual ~ScenarioParser() {}
 
+    static ByteArray StringToArray(std::string input, size_t start, size_t end);
+
 protected:
     virtual void ParseFileLine(std::string& line) = 0;
 
     void ParseFile(std::string& filename);
+
+    static ByteArray ParseRequest(std::string& request);
 };
 
 }
--- source/virt-controller/controller_parser.cpp
+++ source/virt-controller/controller_parser.cpp
@@ -1,9 +1,6 @@
 #include "controller_parser.h"
 
-#include <stdlib.h>
-
 #include <iostream>
-#include <sstream>
 
 #include "functions_vdb.h"
 #include "scenario_conclusion.h"
@@ -20,47 +17,6 @@ ControllerParser::ControllerParser(std::string filename)
     ParseFile(filename);
 }
 
-Byte StringToByte(string& input)
-{
-    int result;
-    stringstream stream;
-    stream << hex << input;
-    stream >> result;
-
-    return result;
-}
-
-ByteArray StringToArray(string input, size_t start, size_t end)
-{
-    if ( input.size() < end )
-        return ByteArray();
-
-    ByteArray result;
-    string byte;
-    for ( size_t i = start; i < end; i+=3 )
-    {
-        byte.append(1, input[i]);
-        byte.append(1, input[i+1]);
-
-        result.push_back(StringToByte(byte));
-
-        byte.clear();
-    }
-
-    return result;
-}
-
-ByteArray ParseRequest(string request)
-{
-    if ( request.empty() )
-        return ByteArray();
-
-    size_t start = 1;
-    size_t end = request.find("]");
-
-    return StringToArray(request, start, end);
-}
-
 ByteArray ParseAnswer(string answer)
 {
     if ( answer.empty() )
@@ -69,12 +25,12 @@ ByteArray ParseAnswer(string answer)
     size_t start = answer.find_last_of("[") + 1;
     size_t end = answer.find_last_of("]");
 
-    return StringToArray(answer, start, end);
+    return ScenarioParser::StringToArray(answer, start, end);
 }
 
 void ControllerParser::ParseFileLine(string& line)
 {
-    ByteArray request(ParseRequest(line));
+    ByteArray request(ScenarioParser::ParseRequest(line));
     ByteArray answer(ParseAnswer(line));
 
     answers_.push_back(pair<ByteArray,ByteArray>(request, answer));
