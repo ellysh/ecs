9147058f757988075305531c3d4a551fd0794d26
Ilya Shpigor <petrsum@gmail.com>
2012-09-10 11:02:16 +0400

    conf: Minor fixes in the configuration file names

--- bin/controller-start-2.sh
+++ bin/controller-start-2.sh
@@ -2,4 +2,4 @@
 
 ./clean.sh
 ./virt-controller --ipl 127.0.0.1 --ipr 127.0.0.1 --portl 4002 --portr 4001 \
-                  --rs 1 --scenario a20-init.vdc > debug.log
+                  --rs 2 --scenario a20-init-2.vdc > debug-2.log
