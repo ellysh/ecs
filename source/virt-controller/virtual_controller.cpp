435d5491ae98a3941f4e2daeac29a0c16dba8358
Ilya Shpigor <petrsum@gmail.com>
2012-09-10 11:36:48 +0400

    VirtualController: Add the ConfigureConnection and CreateProtocol methods

--- source/virt-controller/virtual_controller.cpp
+++ source/virt-controller/virtual_controller.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 
 #include "program_options.h"
-#include "udp_connection.h"
 #include "controller_parser.h"
 #include "protocol_controller.h"
 #include "functions_vdb.h"
@@ -14,29 +13,43 @@ using namespace virt_dashboard;
 static const int kReceiveDelay = 2 * 1000;
 static const int kAnswerDelay = 1;
 
+VirtualController::VirtualController(ProgramOptions& options) : options_(options)
+{
+    ConfigureConnection();
+
+    CreateProtocol();
+}
+
+void VirtualController::ConfigureConnection()
+{
+    connection_.SetLocalPoint(options_.GetString(kIpLocal),
+                             options_.GetInt(kPortLocal));
+    connection_.SetRemotePoint(options_.GetString(kIpRemote),
+                              options_.GetInt(kPortRemote));
+}
+
+void VirtualController::CreateProtocol()
+{
+    ControllerParser parser(options_.GetString(kScenario));
+
+    protocol_ = new ProtocolController(options_.GetInt(kRsAddress),
+                                       parser.GetAnswers());
+}
+
+VirtualController::~VirtualController()
+{
+    delete protocol_;
+}
+
 void VirtualController::Start()
 {
-    /* FIXME: Split this method to submethods */
-    UdpConnection connection;
-    connection.SetLocalPoint(options_.GetString(kIpLocal),
-                             options_.GetInt(kPortLocal));
-    connection.SetRemotePoint(options_.GetString(kIpRemote),
-                              options_.GetInt(kPortRemote));
-
-    ControllerParser* parser = new ControllerParser(options_.GetString(kScenario));
-
-    ProtocolController protocol(options_.GetInt(kRsAddress),
-                                parser->GetAnswers());
-
-    delete parser;
-
     ByteArray request;
     ByteArray answer;
 
     while( true )
     {
         usleep(kReceiveDelay);
-        request = connection.ReceiveData();
+        request = connection_.ReceiveData();
 
         if ( request.empty() )
             continue;
@@ -46,11 +59,11 @@ void VirtualController::Start()
         PrintByteArray(request);
 
         usleep(kAnswerDelay);
-        answer = protocol.GetAnswer(request);
+        answer = protocol_->GetAnswer(request);
 
         cout << "\tanswer = ";
         PrintByteArray(answer);
 
-        connection.SendData(answer);
+        connection_.SendData(answer);
     }
 }
--- source/virt-controller/virtual_controller.h
+++ source/virt-controller/virtual_controller.h
@@ -1,19 +1,32 @@
 #ifndef VIRTUAL_CONTROLLER_H
 #define VIRTUAL_CONTROLLER_H
 
+#include "udp_connection.h"
+#include "macros.h"
+
 namespace virt_dashboard
 {
 
 class ProgramOptions;
+class ProtocolController;
 
 class VirtualController
 {
 public:
-    VirtualController(ProgramOptions& options) : options_(options) {};
+    VirtualController(ProgramOptions& options);
+    ~VirtualController();
+
     void Start();
 
 private:
     ProgramOptions& options_;
+    UdpConnection connection_;
+    ProtocolController* protocol_;
+
+    void ConfigureConnection();
+    void CreateProtocol();
+
+    DISALLOW_COPY_AND_ASSIGN(VirtualController)
 };
 
 }
