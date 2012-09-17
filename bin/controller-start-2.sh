0ea6fd208942011138e6a53250a2abdd8c4cdfbd
Ilya Shpigor <petrsum@gmail.com>
2012-09-17 10:49:25 +0400

    conf: Add complex scenario for pair of a20 controllers

--- bin/controller-start-2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./clean.sh
-./virt-controller --ipl 127.0.0.1 --ipr 127.0.0.1 --portl 4002 --portr 4001 \
-                  --rs 2 --scenario a20-init-2.vdc > debug-2.log
--- bin/controller-start.sh
+++ bin/controller-start.sh
@@ -2,4 +2,4 @@
 
 ./clean.sh
 ./virt-controller --ipl 127.0.0.1 --ipr 127.0.0.1 --portl 4002 --portr 4001 \
-                  --rs 1 --scenario a20-init.vdc > debug.log
+                  --scenario a20-init-pair.vdc > debug.log
