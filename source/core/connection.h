0f2eff5805c1f300276fd5e71fd8ea8880a36ae0
Ilya Shpigor <petrsum@gmail.com>
2013-03-12 14:28:50 +0400

    Add the Connection abstract class

---
+++ source/core/connection.h
@@ -0,0 +1,22 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+#include "types_vdb.h"
+
+namespace virt_dashboard
+{
+
+class Connection
+{
+public:
+    Connection() {};
+    virtual ~Connection() {};
+
+    virtual ByteArray ReceiveData() = 0;
+    virtual void SendData(const ByteArray& data) = 0;
+    virtual void Configure(const ProgramOptions& options) = 0;
+};
+
+}
+
+#endif
