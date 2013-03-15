376b7734444acaffa184ed6ee44064108e6301d4
Ilya Shpigor <petrsum@gmail.com>
2013-03-15 15:32:06 +0400

    SerialConnection: Stub implementation of the virtual methods cmake: Add dependency from the libserial library

--- source/core/serial_connection.cpp
+++ source/core/serial_connection.cpp
@@ -5,3 +5,25 @@
 using namespace std;
 using namespace virt_dashboard;
 
+SerialConnection::~SerialConnection()
+{
+    if ( connection_ != NULL )
+        delete connection_;
+}
+
+ByteArray SerialConnection::ReceiveData()
+{
+    /* FIXME: Implement this method */
+    return ByteArray();
+}
+
+void SerialConnection::SendData(const ByteArray& data)
+{
+    /* FIXME: Implement this method */
+}
+
+void SerialConnection::Configure(const ProgramOptions& options)
+{
+    /* FIXME: Implement this method */
+}
+
--- source/core/serial_connection.h
+++ source/core/serial_connection.h
@@ -1,8 +1,10 @@
-#ifndef SERIAL_CONNECTION_H
-#define SERIAL_CONNECTION_H
+#ifndef VD_SERIAL_CONNECTION_H
+#define VD_SERIAL_CONNECTION_H
 
 #include "connection.h"
 #include "types_vdb.h"
+
+#include <libserial/serial_connection.h>
 
 namespace virt_dashboard
 {
@@ -10,14 +12,15 @@ namespace virt_dashboard
 class SerialConnection : public Connection
 {
 public:
-    SerialConnection() {};
-    virtual ~SerialConnection() {};
+    SerialConnection() : connection_(NULL) {};
+    virtual ~SerialConnection();
 
     virtual ByteArray ReceiveData();
     virtual void SendData(const ByteArray& data);
     virtual void Configure(const ProgramOptions& options);
 
 private:
+    serial::SerialConnection* connection_;
 };
 
 }
