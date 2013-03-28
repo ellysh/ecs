a6a5e8d281c01f48b3bc3db432e99e9acb094a43
Ilya Shpigor <petrsum@gmail.com>
2013-03-28 14:42:04 +0400

    Add the testing scenario for A20 controllers Fix cmd options in the scripts for testing launching

--- bin/controller-serial.sh
+++ bin/controller-serial.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./clean.sh
-./virt-controller --dev /dev/ --portr 4001 \
-                  --scenario a20-init-pair.vdc --type udp > debug.log
+./virt-controller --dev /dev/ttyUSB0 --baud 57600 \
+                  --scenario a20-init-pair.vdc --type serial > debug.log
